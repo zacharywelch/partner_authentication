d761a2157cc25eeae8cf64af5145243dd56483e9
zwelchcb <Zachary.Welch@careerbuilder.com>
2015-08-05 17:13:47 -0400

    update authorized ip template

--- lib/generators/partner_authentication/templates/authorized_ip.rb
+++ lib/generators/partner_authentication/templates/authorized_ip.rb
@@ -3,7 +3,7 @@ class AuthorizedIp < ActiveRecord::Base
 
   alias_attribute :ip_address, :IPAddress
   
-  belongs_to :api_key, foreign_key: :PartnerID
+  belongs_to :api_key, primary_key: :PartnerID, foreign_key: :PartnerID
 
   def pattern
     Regexp.new ip_address.gsub('.', '\\.').gsub('*', '\\d{1,3}')
